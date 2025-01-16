50aa997788c313c8b8ba8f8291694ea1a94c898e
noyansoylu <noyansoylu1@gmail.com>
2025-01-17 01:00:35 +0300

    finished

---
+++ Week10/BillGates/src/data.js
@@ -0,0 +1,45 @@
+export const items = [
+    {
+      id: 1,
+      name: 'Big Mac',
+      price: 5,
+      image: '/assets/big-mac.jpg',
+      count: 0,
+    },
+    {
+      id: 2,
+      name: 'Book',
+      price: 15,
+      image: '/assets/book.jpg',
+      count: 0,
+    },
+    {
+      id: 3,
+      name: 'Year of Netflix',
+      price: 100,
+      image: '/assets/year-of-netflix.jpg',
+      count: 0,
+    },
+    {
+      id: 4,
+      name: 'Mansion',
+      price: 50000000,
+      image: '/assets/mansion.jpg',
+      count: 0,
+    },
+    {
+      id: 5,
+      name: 'Mona Lisa',
+      price: 780000000,
+      image: '/assets/mona-lisa.jpg',
+      count: 0,
+    },
+    {
+      id: 6,
+      name: 'NBA Team',
+      price: 2120000000,
+      image: '/assets/nba-team.jpg',
+      count: 0,
+    }
+  ];
+  
